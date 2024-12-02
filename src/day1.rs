bed00d7f00c535f43e494ebdd0944c868f0792dd
Tristan Bony <tristan.bony@hotmail.fr>
2024-12-01 23:15:34 -0600

    Add day 1

---
+++ src/day1.rs
@@ -0,0 +1,69 @@
+pub fn day1_1() {
+    let (mut a, mut b) = read_from_file();
+
+    a.sort();
+    b.sort();
+
+    println!("Day 1-1: {}", distance(a, b));
+}
+
+pub fn day1_2() {
+    let (a, b) = read_from_file();
+
+    println!("Day 1-2: {}", similarity(a, b));
+}
+
+fn read_from_file() -> (Vec<u32>, Vec<u32>) {
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+
+    let file =
+        std::fs::File::open("/home/tristan/Documents/Projects/advent_of_code/inputs/day1.txt")
+            .unwrap();
+    let reader = std::io::BufReader::new(file);
+
+    for line in std::io::BufRead::lines(reader) {
+        let line = line.unwrap();
+        let mut nums = line.split_whitespace();
+
+        a.push(nums.next().unwrap().parse().unwrap());
+        b.push(nums.next().unwrap().parse().unwrap());
+    }
+
+    (a, b)
+}
+
+fn distance(a: Vec<u32>, b: Vec<u32>) -> u32 {
+    if a.len() != b.len() {
+        panic!("Arrays must be the same length.");
+    }
+
+    let mut sum = 0;
+    for i in 0..a.len() {
+        if a[i] < b[i] {
+            sum += b[i] - a[i];
+        } else {
+            sum += a[i] - b[i];
+        }
+    }
+
+    sum
+}
+
+fn similarity(a: Vec<u32>, b: Vec<u32>) -> u32 {
+    let mut sum = 0;
+    for ai in a {
+        sum += ai * count_value(b.clone(), ai);
+    }
+    sum
+}
+
+fn count_value(v: Vec<u32>, n: u32) -> u32 {
+    let mut sum = 0;
+    for i in v {
+        if i == n {
+            sum += 1;
+        }
+    }
+    sum
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+pub mod day1;
+
+use crate::day1::{day1_1, day1_2};
+
+fn main() {
+    println!("BEGIN Day 1");
+    day1_1();
+    day1_2();
+    println!("END Day 1")
+}
